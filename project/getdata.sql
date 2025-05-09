fa049409c27f25f205ee871dcafca6cae41215db
Hayley Boll <hales.boll@gmail.com>
2025-05-09 16:19:47 -0500

    Update getdata.sql

--- project/getdata.sql
+++ project/getdata.sql
@@ -1,1 +1,15 @@
+-- Here is how I would get the data for this report if it was in Snowflake instead of excel.
+SELECT
 
+Vendor "Vendor Name",
+Invoice,
+Line Item,
+Description "Item Description",
+Amount "Unit Cost",
+Quantity,
+Date Purchased,
+Category,
+
+FROM DATABASE.SCHEMA.InvoiceTable
+
+WHERE 'Date Purchased' > 20200101
